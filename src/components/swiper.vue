60dd8614d65ac5583728b657546061fe91d0ba83
limlah <limlah@live.com>
2019-04-29 00:07:55 +0800

    向swiper组件添加vue-awesome-swiper插件

--- src/components/swiper.vue
+++ src/components/swiper.vue
@@ -1,17 +1,43 @@
 <template>
   <div class="container">
-    <p>01010101</p>
-    <p>01010101</p>
-    <p>01010101</p>
+    <swiper :options="swiperOption">
+      <swiper-slide>I'm Slide 1</swiper-slide>
+      <swiper-slide>I'm Slide 2</swiper-slide>
+      <swiper-slide>I'm Slide 3</swiper-slide>
+      <swiper-slide>I'm Slide 4</swiper-slide>
+      <swiper-slide>I'm Slide 5</swiper-slide>
+      <swiper-slide>I'm Slide 6</swiper-slide>
+      <swiper-slide>I'm Slide 7</swiper-slide>
+
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
+import { swiper, swiperSlide } from "vue-awesome-swiper";
 export default {
-  
-}
+  name: "carrousel",
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: ".swiper-pagination"
+        },
+        autoplay: {
+          delay: 2000,
+          disableOnInteraction: false
+        },
+        loop: true
+      }
+    };
+  }
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
