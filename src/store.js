6961c319ea4c971faedd033fefca999d85039484
limlah <limlah@live.com>
2019-04-29 00:44:46 +0800

    设置state

--- src/store.js
+++ src/store.js
@@ -5,7 +5,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    top_stories: [],  // swiper组件中需要用到的数据
+    today_stories: [], // 当日新闻
+    date: '', // 日期
+    before_stories: [], // 今天之前的新闻
   },
   mutations: {
 
